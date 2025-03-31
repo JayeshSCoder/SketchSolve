d44b019ad294ab2d98ef7d3e60dd48a7828c4d63
JayeshSCoder <jayesh.chaudhari.coder234@gmail.com>
2025-03-31 11:16:32 +0530

    Updated tailwind css cdn and title

--- sketch-solve/src/Components/NavbarTop/NavbarTop.jsx
+++ sketch-solve/src/Components/NavbarTop/NavbarTop.jsx
@@ -8,7 +8,7 @@ export default function NavbarTop() {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative text-white text-2xl font-bold flex h-16 items-center justify-center">
 
-          Welcome To Draw and Solve
+          Welcome To SketchSolve
 
         </div>
       </div>
